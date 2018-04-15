02a1605b76a9a55e6c2a3937dacef0cf263e2c6a
alsadi <alsadi@192.168.100.2>
2018-04-16 00:21:47 +0300

    Fixing width and height

--- src/wmsn/Node.java
+++ src/wmsn/Node.java
@@ -3,4 +3,9 @@ package wmsn;
 public class Node {
 public int id;
 public Information info;
+public final int CAR_NODE = 300; // indicates the node type & Range of communication
+public final int SINK_NODE = 500;
+public final int ROADSIDE_NODE = 140;
+public int type;
+public int range;
 }
--- src/wmsn/Plane.java
+++ src/wmsn/Plane.java
@@ -1,23 +1,25 @@
 package wmsn;
 
 public class Plane {
-	public static Coordinates [][] plane = new Coordinates[100][100];
+	public final static int width = 1000;
+	public final static int height = 2000;
+	public static Coordinates [][] plane = new Coordinates[height][width];
 
 	public Plane()
 	{
-		for(int x = 0; x < 100; x++)
-			for(int y = 0; y < 100; y++)
-				plane[y][x] = new Coordinates(x,y);
+		for(int x = 0; x < height; x++)
+			for(int y = 0; y < width; y++)
+				plane[x][y] = new Coordinates(x,y);
 	}
 	
 	
 	public void show()
 	{
-		for(int x = 0; x < 100; x++)
+		for(int x = 0; x < height; x++)
 		{
-			for(int y = 0; y < 100; y++)
+			for(int y = 0; y < width; y++)
 				System.out.print("["+plane[x][y].x +","+ plane[x][y].y+","+ plane[x][y].content+"]    ");
-		System.out.println();
+		System.out.println("done");
 		}
 		
 	}
